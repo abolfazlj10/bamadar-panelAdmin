00c92704302d5034c9487a28cb899e94531b4d90
abolfazl <abolfazljamshidi101010@gmail.com>
2025-06-01 11:59:29 +0330

    remove border from logos's access card.

--- packages/web/src/components/accessCard.jsx
+++ packages/web/src/components/accessCard.jsx
@@ -14,7 +14,7 @@ export default function AccessCard ({data,id}) {
         <motion.div
         className="h-[500px] w-[600px] bg-[#fff] shadow-xl rounded-3xl px-10 py-6 grid grid-rows-[1fr_2fr_1fr] text-center border duration-300 hover:shadow-2xl hover:-translate-y-1 hover:border-blue-500">
             <div className="flex justify-center items-center">
-                <div className="flex justify-center items-center rounded-full w-[150px] h-[150px] p-5 ring-blue-600 ring-2">
+                <div className="flex justify-center items-center rounded-full w-[150px] h-[150px] p-5">
                     <motion.img initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 1 }} transition={{ duration: 0.1 }} layoutId={`icon-${data.id}`} className="w-full" src={`https://basesuperapp.bamadar.com/v1/media/${data.image}`} />
                 </div>
             </div>
